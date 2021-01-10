fc0821539ec77799e4e7207b4cc5b04f43279290
Lewis Luck <lluck@MacBook-Pro-70.gateway>
2021-01-10 16:30:30 +1000

    Further aesthetic changes. Implemented Coreplus API, autocomplete, client.

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 #Flask libraries
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify, Response
 from static.helpers import Form, Client, Product
+import requests
 
 #Standard libraries
 import os
 import json
+import jwt
+import datetime
+
 
 #Open file for secret
-secret = open("secret.txt", "r").readlines()
+secret = open("secret.txt", "r").read().splitlines()
 
 #Coreplus API keys
 COREPLUS_API_CONSUMER_ID = secret[0]
 COREPLUS_API_SECRET = secret[1]
 COREPLUS_ACCESS_KEY = secret[2]
+
+#Coreplus base url
+COREPLUS_BASE_URL = "https://sandbox.coreplus.com.au/api/core/v2.1"
 
 # *** TESTING ***
 """
@@ -42,7 +49,36 @@ def index():
 @app.route("/contracts", methods = ["GET", "POST"])
 def contracts():
     if request.method == "GET":
-        return render_template("contracts.html")
+        return render_template("contracts.html", secret=secret)
+    else:
+        if request.form:
+            print("hello")
+
+@app.route("/get_clients", methods= ["GET", "POST"])
+def get_clients():
+    name_fragment = request.args.get('name')
+    if name_fragment:
+        client_list_query = COREPLUS_BASE_URL + "/Client/?name=" + name_fragment
+        claims = {
+              "iss": "http://127.0.0.1:5000/",
+              "aud": "https://coreplus.com.au",
+              "nbf": datetime.datetime.utcnow(),
+              "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=60),
+              "consumerId": COREPLUS_API_CONSUMER_ID,
+              "accessToken": COREPLUS_ACCESS_KEY,
+              "url": client_list_query,
+              "httpMethod": "GET"
+          };
+        encoded_jwt_byte = jwt.encode(claims, COREPLUS_API_SECRET, algorithm='HS256')
+        headers = {"Authorization": "JwToken" + " " + str(encoded_jwt_byte), "content-type": "application/json"}
+        response = requests.get(url=client_list_query, verify=True, headers=headers, timeout=45);
+        if response.json():
+            return jsonify(response.json()["clients"])
+        else:
+            return jsonify([])
+    else:
+        return render_template("error.html", message="Something went wrong accessing Coreplus!", address="/contracts")
+
 
 @app.route("/deliveries", methods = ["GET", "POST"])
 def deliveries():
@@ -56,7 +92,5 @@ def reports():
 def login():
     return render_template("error.html", message="This page is not implemented yet.", address="/")
 
-"""
 if __name__ == "__main__":
     app.run(debug=True)
-"""
